7015e5488493bc6051a7f78c1cc767d71ffa3c84
Dipak Pathak <dipak@techasoft.com>
2025-04-04 11:05:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>This is a simple React Vite application.</p>
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <header>
+        <h1>Welcome to My Vite App</h1>
+      </header>
+    );
+  };
+  export default Header;
+  
